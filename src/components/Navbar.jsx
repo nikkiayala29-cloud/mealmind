adcd569c88471a61be0dc19819297701900804f8
nikkiayala29-cloud <nikkiayala29@gmail.com>
2026-05-31 02:32:36 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,8 @@
-
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>🥗 MealMind</h1>
+      <p>Plan meals smarter. Eat better. Live healthier.</p>
+    </div>
+  );
+}
