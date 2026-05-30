2df56c9cb91e7dc6fee336c3089150ef409a63f9
nikkiayala29-cloud <nikkiayala29@gmail.com>
2026-05-31 02:31:36 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import Navbar from "./components/Navbar";
+import MealPlanner from "./components/MealPlanner";
+import GroceryList from "./components/GroceryList";
+import Nutrition from "./components/Nutrition";
 
+export default function App() {
+  return (
+    <div className="app">
+      <Navbar />
+
+      <div className="grid">
+        <MealPlanner />
+        <Nutrition />
+        <GroceryList />
+      </div>
+    </div>
+  );
+}
