3c0a70aa375e8a3290672f1b3c3fe56309855c0a
nikkiayala29-cloud <nikkiayala29@gmail.com>
2026-05-31 02:33:33 +0800

    Update recipes.js

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -1,1 +1,20 @@
-
+export const recipes = [
+  {
+    name: "Chicken Salad",
+    calories: 350,
+    protein: 30,
+    ingredients: ["Chicken", "Lettuce", "Olive Oil"]
+  },
+  {
+    name: "Beef Rice Bowl",
+    calories: 550,
+    protein: 40,
+    ingredients: ["Beef", "Rice", "Egg"]
+  },
+  {
+    name: "Oatmeal Banana",
+    calories: 300,
+    protein: 10,
+    ingredients: ["Oats", "Banana", "Milk"]
+  }
+];
